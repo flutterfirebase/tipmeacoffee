b480ca8b6c325c633047384670b6256be311ec53
NickBird <almabroral@gmail.com>
2022-02-05 21:01:50 +0500

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -170,10 +170,12 @@ router.post('/share', async (req, res) => {
       if(helper.sites.includes(domainName)){res.send({ error: true, message: 'Sharing from this url is not allowed' });
       } else {
         Meta.parser(post.url, function (err, result) {
-          if(result){let meta = result['og']; 
-            if(!meta.title){res.send({ error: true, message: 'Phew.. Unable to fetch shared link' });
-            } else {res.send({ error: false, meta: meta, link: domainName });}
-          } else {res.send({ error: true, message: 'Unable to Parse URL' });}
+          try{
+            if(result){let meta = result['og']; 
+              if(!meta.title){res.send({ error: true, message: 'Phew.. Unable to fetch shared link' });
+              } else {res.send({ error: false, meta: meta, link: domainName });}
+            } else {res.send({ error: true, message: 'Unable to Parse URL' });}
+          } catch (err) { console.log(err); res.send({ error: true, message: err }) }
         }) 
       }
     }
